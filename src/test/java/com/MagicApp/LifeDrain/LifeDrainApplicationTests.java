37c4f296626974db255e45470baed3482a266141
Omar-Martinez-F <omartinezfuentes@csumb.edu>
2026-07-17 20:24:27 -0700

    Added springboot to file

---
+++ src/test/java/com/MagicApp/LifeDrain/LifeDrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MagicApp.LifeDrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeDrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
